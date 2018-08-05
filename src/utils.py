c33d15e8d0247bf351b2e1f090f5b21bd12537da
Shayne O'Brien <shayneob@mit.edu>
2018-08-05 15:05:52 -0400

    need to fix grad problem...

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,6 @@ def to_var(x):
     """ Make a tensor cuda-erized and requires gradient """
     return to_cuda(x).requires_grad_()
 
-
 def to_cuda(x):
     """ Cuda-erize a tensor """
     if torch.cuda.is_available():
